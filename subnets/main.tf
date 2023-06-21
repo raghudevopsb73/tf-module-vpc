de4fbb5c94cd32815ec16cabfeabd29204c9aff2
Raghu K <raghuk.vit@gmail.com>
2023-06-21 08:39:37 +0530

    Update tf-module-vpc/

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "main" {
-  count                = length(var.cidr_block)
-  vpc_id               = var.vpc_id
-  cidr_block           = element(var.cidr_block, count.index)
-  availability_zone_id = element(var.az, count.index)
+  count             = length(var.cidr_block)
+  vpc_id            = var.vpc_id
+  cidr_block        = element(var.cidr_block, count.index)
+  availability_zone = element(var.az, count.index)
 
   tags = merge({
     Name = "${var.env}-${var.subnet_name}-subnet"
