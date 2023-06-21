7b29d15fb249319b05538e50f0fcba7b5082daa4
Raghu K <raghuk.vit@gmail.com>
2023-06-21 08:27:03 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 resource "aws_vpc" "main" {
   cidr_block         = var.cidr_block
   enable_dns_support = true
+  tags = merge({
+    Name = "${var.env}-vpc"
+    },
+  var.tags)
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,1 +1,3 @@
 variable "cidr_block" {}
+variable "tags" {}
+variable "env" {}
