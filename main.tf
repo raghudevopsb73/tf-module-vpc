aaed90615217938cda51f6763091188e2c22a043
Raghu K <raghuk.vit@gmail.com>
2023-06-22 08:05:35 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -22,6 +22,6 @@ module "subnets" {
 }
 
 resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        =
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = var.default_vpc_id
 }
