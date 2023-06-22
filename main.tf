2d3451dd4a3a4b6c924e0151daea32d80976c6df
Raghu K <raghuk.vit@gmail.com>
2023-06-22 08:50:52 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -36,3 +36,9 @@ resource "aws_internet_gateway" "igw" {
   var.tags)
 }
 
+resource "aws_route" "route-igw" {
+  route_table_id         = module.subnets["public"].route_table_ids
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
